308d7abeee70eab12a01886353ca2bfbae417007
Zach Terault <145575835+teraultz@users.noreply.github.com>
2025-12-08 11:37:32 -0500

    Almost done

--- Assets/Scripts/GM4.cs
+++ Assets/Scripts/GM4.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GM4 : MonoBehaviour
 {
+    public GameObject restartcanvas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,10 @@ public class GM4 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(0);
     }
 }
